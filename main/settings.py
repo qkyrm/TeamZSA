3971a5bc826f9414f706110280c18fed4c4efbee
Zinaida <zinaidasataralieva@gmail.com>
2023-11-27 23:26:37 +0600

    3 commit

--- main/settings.py
+++ main/settings.py
@@ -18,6 +18,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Application definition
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
 from django.shortcuts import render
+class Register(View):
+    template_name = 'registration/register.html'
 
-# Create your views here.
+    def get(self, request):
+        content = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.template_name, content)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            return redirect('home')
+        context = {
+            'form':form
+
+        }
+        return render(request, self.template_name, context)
+
