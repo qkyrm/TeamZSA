a262a1140c49206a7b0b15b8b97f48225dc36754
Saida <saidaurmanbetova555@gmail.com>
2023-12-07 11:07:44 +0600

    Saida's second

--- sweatshirts/views.py
+++ sweatshirts/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import SweatShirts
 
 from . import models
-def ThingsListView(request):
+def SweatListView(request):
     shirts = models.SweatShirts.objects.all()
     context = {
         'shirts_key': shirts,
 
     }
-    html_name = 'sweatshirts/sweatshirts.html'
+    html_name = 'things/page2.html'
     return render(request, html_name, context)
+def SweatDeteilView(request, id):
+    if request.method == "GET":
+        show_id_key = get_object_or_404(SweatShirts, id=id)
+        return  render(request, template_name='things/things_detail.html', context={"show_id_key":show_id_key})
+
