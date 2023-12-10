c740ed76caa43ecc9c70b8761f2446ed753805e3
Zinaida <zinaidasataralieva@gmail.com>
2023-12-10 16:31:02 +0600

    Zinka's code

--- users/views.py
+++ users/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from .forms import UserRegistrationForm  # Исправлен импорт
 
 @login_required
 def dashboard(request):
     return render(request, 'registration/dashboard.html', {'section': 'dashboard'})
-def change_password_view(request):
+
+def register(request):
     if request.method == 'POST':
-        # Обработка формы изменения пароля
-        # ...
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        # Если метод запроса не POST, инициализируем пустую форму
+        user_form = UserRegistrationForm()
 
-        # Если изменение пароля прошло успешно, добавьте сообщение
-        messages.success(request, 'Password changed successfully.')
-
-        return redirect('dashboard')  # Или куда-то еще, куда вы хотите перенаправить пользователя после изменения пароля
-
-    return render(request, 'your_template.html')
+    # Если форма не прошла валидацию или это GET-запрос, отображаем форму
+    return render(request, 'registration/register.html', {'user_form': user_form})
